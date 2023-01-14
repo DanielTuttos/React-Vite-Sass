1f76ec60a98d7a56574dd15fa3a5316e8753e9ed
Daniel Romero <tutorialesdanielromero@gmail.com>
2023-01-14 16:04:05 -0500

    ejemplo vh vw

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,56 @@ import Buttons from "./components/Buttons";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="title">Hola desde sass</h1>
-      <hr />
-      <ul>
-        <li>
-          <a href="#">Google</a>
-        </li>
-        <li>
-          <a href="#">facebook</a>
-        </li>
-        <li>
-          <a href="#">Instagram</a>
-        </li>
-      </ul>
-      <hr />
-      <h2>Counter: 0</h2>
-      <Buttons type={'btn btn-primary'} text={'+1'}/>
+    // <div>
+    //   <h1 className="title">Hola desde sass</h1>
+    //   <hr />
+    //   <ul>
+    //     <li>
+    //       <a href="#">Google</a>
+    //     </li>
+    //     <li>
+    //       <a href="#">facebook</a>
+    //     </li>
+    //     <li>
+    //       <a href="#">Instagram</a>
+    //     </li>
+    //   </ul>
+    //   <hr />
+    //   <h2>Counter: 0</h2>
+    //   <Buttons type={'btn btn-primary'} text={'+1'}/>
+    // </div>
+    <div className="container_item">
+      <div className="hijo">
+        <div className="hijo_hijo">
+          asdjhakjs
+          asdasd
+          asdasdsdfasdf
+          asdasdsdfasdfasdf
+          asdasd
+          asdasdsdfasdfasdf
+
+          asdasd
+
+          asdasd
+          <p>hola</p>
+          <p>hola</p>
+          <p>hola</p>
+          <p>hola</p>
+          <p>hola</p>
+          <p>hola</p>
+          <p>hola</p>
+          <p>hola</p>
+          <p>hola</p>
+          <p>hola</p>
+          <p>hola</p>
+          <p>hola</p>
+          <p>hola</p>
+          <p>hola</p>
+          <p>hola</p>
+          <p>hola</p>
+          <p>hola</p>
+        </div>
+      </div>
     </div>
   );
 };

95aa30c864f03842ee9b03e23901983a7bb78615
Daniel Romero <tutorialesdanielromero@gmail.com>
2023-01-14 15:16:27 -0500

    anidando elementos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div>
-      <h1 className='title'>Hola desde sass</h1>
+      <h1 className="title">Hola desde sass</h1>
+      <hr />
+      <ul>
+        <li>
+          <a href="#">Google</a>
+        </li>
+        <li>
+          <a href="#">facebook</a>
+        </li>
+        <li>
+          <a href="#">Instagram</a>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
